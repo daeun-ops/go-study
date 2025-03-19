6d6ae5e4efc7adf23e8e9666d58a2b610189d978
Daeun Kim <euncd61@gmail.com>
2025-03-20 00:02:53 +0900

    Update ex4.1.go

--- variable/ex4.1/ex4.1.go
+++ variable/ex4.1/ex4.1.go
@@ -1,1 +1,20 @@
+package main
 
+import "fmt"
+
+func main() {
+    var a int = 10 
+    var msg string = "hi all variabe"
+
+    a = 20
+    msg = "Good daeun"
+    fmt.Println(msg, a)
+// 모듈 만들어주고
+    //go mod init goprojects/ex4.1.
+
+// 실행 파일이 떨어짐
+    // go build 
+
+// 실행 명령어 
+    // ./ex4.1.exe 
+}
