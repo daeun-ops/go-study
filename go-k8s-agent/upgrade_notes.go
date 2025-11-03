230bed6af88515ba4807beadd8da2c3fe01e5d05
daeun-ops <euncd61@gmail.com>
2025-11-03 15:26:01 +0900

    perf(k8s): optimize metrics scraping interval using exponential backoff

--- go-k8s-agent/upgrade_notes.go
+++ go-k8s-agent/upgrade_notes.go
@@ -1,1 +1,2 @@
 // Mon Nov  3 15:26:01 KST 2025 - feat(agent): implement adaptive CPU throttling for sidecar containers
+// Mon Nov  3 15:26:01 KST 2025 - perf(k8s): optimize metrics scraping interval using exponential backoff
