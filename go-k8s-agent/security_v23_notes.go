7190ccf15bf31f68f20f637b91825dac00f31a45
daeun-ops <euncd61@gmail.com>
2025-11-03 15:38:04 +0900

    chore(supply-chain): generate SBOM using Syft for all Go modules

--- go-k8s-agent/security_v23_notes.go
+++ go-k8s-agent/security_v23_notes.go
@@ -1,2 +1,3 @@
 // Mon Nov  3 15:38:04 KST 2025 - feat(security): integrate OPA Gatekeeper policies for namespace restrictions
 // Mon Nov  3 15:38:04 KST 2025 - feat(policy): add Kyverno rule to enforce image tag immutability
+// Mon Nov  3 15:38:04 KST 2025 - chore(supply-chain): generate SBOM using Syft for all Go modules
