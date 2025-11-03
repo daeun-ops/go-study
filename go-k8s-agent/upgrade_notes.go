0598974dec987dfc392ee4d672fd6e91897ed111
daeun-ops <euncd61@gmail.com>
2025-11-03 15:26:01 +0900

    feat(agent): support leader election using Lease API

--- go-k8s-agent/upgrade_notes.go
+++ go-k8s-agent/upgrade_notes.go
@@ -11,3 +11,4 @@
 // Mon Nov  3 15:26:01 KST 2025 - feat(tracing): add OpenTelemetry spans for agent sync cycle
 // Mon Nov  3 15:26:01 KST 2025 - chore(logging): switch zap logger to structured JSON mode
 // Mon Nov  3 15:26:01 KST 2025 - fix(controller): handle nil pointer panic on pod eviction
+// Mon Nov  3 15:26:01 KST 2025 - feat(agent): support leader election using Lease API
