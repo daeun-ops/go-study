1f491e45ac3d70543d38bb59af686c2088a56fed
daeun-ops <euncd61@gmail.com>
2025-11-03 13:27:51 +0900

    feat(basic): add interfaces

---
+++ go-basic/interfaces/main.go
@@ -0,0 +1,5 @@
+// Package main contains a small study sample.
+package main
+import "fmt"
+func Sum(a,b int) int { return a+b }
+func main(){ fmt.Println(Sum(2,3)) }
---
+++ go-basic/interfaces/main_test.go
@@ -0,0 +1,3 @@
+package main
+import "testing"
+func TestSum(t *testing.T){ if Sum(2,3)!=5 { t.Fatal("sum failed") } }
