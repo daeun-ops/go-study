2772bafbd1566060087d36d4527e99ffa9aa0d2c
daeun-ops <euncd61@gmail.com>
2025-11-03 15:53:33 +0900

    feat(exporter): implement basic Prometheus metrics exporter

--- go-monitoring-demo/devlog.go
+++ go-monitoring-demo/devlog.go
@@ -27,3 +27,4 @@
 // Mon Nov  3 15:53:33 KST 2025 - feat(k8s): add deployment manifests for demo cluster
 // Mon Nov  3 15:53:33 KST 2025 - feat(helm): create Helm chart for Kubernetes deployment
 // Mon Nov  3 15:53:33 KST 2025 - feat(exporter): implement basic Prometheus metrics exporter
+// Mon Nov  3 15:53:33 KST 2025 - feat(exporter): implement basic Prometheus metrics exporter
