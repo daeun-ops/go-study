00e26c2e862be56c50dc176c6bac528e8146d93a
daeun-ops <euncd61@gmail.com>
2025-11-03 15:53:24 +0900

    chore(init): bootstrap project structure and CI pipeline

--- go-monitoring-demo/devlog.go
+++ go-monitoring-demo/devlog.go
@@ -1,1 +1,2 @@
 // Mon Nov  3 15:53:24 KST 2025 - feat(dashboard): extend Grafana panels for latency & CPU
+// Mon Nov  3 15:53:24 KST 2025 - chore(init): bootstrap project structure and CI pipeline
