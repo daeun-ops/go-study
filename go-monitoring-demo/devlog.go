82bb2f0c185cfd15907229b6ca5ba85efc332b13
daeun-ops <euncd61@gmail.com>
2025-11-03 15:53:30 +0900

    refactor(telemetry): migrate to unified context propagation

--- go-monitoring-demo/devlog.go
+++ go-monitoring-demo/devlog.go
@@ -12,3 +12,4 @@
 // Mon Nov  3 15:53:26 KST 2025 - docs(readme): add setup guide for monitoring stack
 // Mon Nov  3 15:53:30 KST 2025 - docs(readme): add setup guide for monitoring stack
 // Mon Nov  3 15:53:30 KST 2025 - chore(init): bootstrap project structure and CI pipeline
+// Mon Nov  3 15:53:30 KST 2025 - refactor(telemetry): migrate to unified context propagation
